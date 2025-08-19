c400f06a996b84ba47583aa2115a3ab469b29a71
sirius4galaxy <uniswap_v2@163.com>
2025-08-19 09:36:41 +0800

    add comments to grab.cisum

--- contracts/grab.cisum/include/grab.cisum.hpp
+++ contracts/grab.cisum/include/grab.cisum.hpp
@@ -26,9 +26,28 @@ public:
 
   ~grab_cisum() { _global.set(_gstate, get_self()); }
 
+  /**
+   * Initialize the contract and set the admin account.
+   * Only contract self can call this action.
+   *
+   * @param admin                The account to be set as admin.
+   */
   [[eosio::action]]
   void init(const eosio::name& admin);
 
+  /**
+   * Create a new rush sale event.
+   * Only admin can call this action.
+   *
+   * @param show_id              The show identifier.
+   * @param ticket_id            The ticket identifier.
+   * @param started_at           Sale start time.
+   * @param ended_at             Sale end time.
+   * @param price                Ticket price.
+   * @param max_grabs_per_user   Max grabs per user.
+   * @param win_ratio            Win ratio (1-10000).
+   * @param total_tickets        Total tickets available.
+   */
   [[eosio::action]]
   void addrushsale(   nsymbol        show_id,
                       nsymbol        ticket_id,
@@ -40,19 +59,50 @@ public:
                       uint32_t       total_tickets
    );
 
+  /**
+   * Delete a rush sale event.
+   * Only admin can call this action.
+   *
+   * @param rush_sale_id         The rush sale id to delete.
+   * @param forced               If true, force delete even if tickets sold.
+   */
   [[eosio::action]]
   void delrushsale( uint64_t rush_sale_id, bool forced );
-
+  /**
+   * Update rush sale parameters. Only admin can call.
+   * Each parameter is optional and only updated if provided.
+   *
+   * @param rush_sale_id         The rush sale id to update.
+   * @param win_ratio            Optional new win ratio.
+   * @param max_grabs_per_user   Optional new max grabs per user.
+   * @param total_tickets        Optional new total tickets.
+   * @param ended_at             Optional new end time.
+   */
   [[eosio::action]]
-  void updrushsale( uint64_t rush_sale_id,
-                    std::optional<uint32_t> win_ratio,
-                    std::optional<uint32_t> total_tickets,
+  void updrushsale( uint64_t                  rush_sale_id,
+                    std::optional<uint32_t>   win_ratio,
+                    std::optional<uint32_t>   total_tickets,
                     std::optional<time_point> ended_at
   );
 
+  /**
+   * Batch delete users for a rush sale. Only admin can call.
+   *
+   * @param rush_sale_id         The rush sale id.
+   * @param max_count            Max number of users to delete in one call.
+   */
   [[eosio::action]]
   void delusers( uint64_t rush_sale_id, uint32_t max_count );
 
+  /**
+   * Handle incoming token transfer for grabbing tickets.
+   * Called automatically on transfer from nestar.cisum.
+   *
+   * @param from                 Sender account.
+   * @param to                   Receiver (contract self).
+   * @param quantity             Amount transferred.
+   * @param memo                 Memo string, format: "grab:<rush_sale_id>".
+   */
   [[eosio::on_notify("nestar.cisum::transfer")]]
   void on_transfer(const name& from, const name& to, const asset& quantity, const string& memo);
 
