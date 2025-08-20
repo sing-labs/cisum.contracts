4bebeddecc722098c8f503ff863e3eea2015e86e
sirius4galaxy <uniswap_v2@163.com>
2025-08-20 16:49:49 +0800

    refine nsymbol related code in cvbadgestore contract

--- contracts/cvbadgestore/include/flon.ntoken.hpp
+++ contracts/cvbadgestore/include/flon.ntoken.hpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <string>
 #include <set>
+#include <flon/nasset.hpp>
 
 
 static constexpr eosio::name active_perm{"active"_n};
@@ -19,64 +20,6 @@ using std::string;
 using std::vector;
 
 using namespace eosio;
-
-static constexpr uint32_t U1E9  = 10'0000'0000UL;
-
-struct nsymbol {
-    uint32_t id;
-    uint32_t pid;
-
-    nsymbol() {}
-    nsymbol(const uint32_t& i): id(i),pid(0) {}
-    nsymbol(const uint32_t& i, const uint32_t& p): id(i),pid(p) {
-        check( pid < U1E9, "pid must be below 10**9" );
-        check( id < U1E9, "id must be below 10**10" );
-    }
-
-    nsymbol(const uint64_t& raw) {
-        check( pid < U1E9, "pid must be below 10**9" );
-        check( id < U1E9, "id must be below 10**10" );
-
-        pid = raw / U1E9;
-        id  = raw - pid * U1E9;
-    }
-
-    friend bool operator==(const nsymbol&, const nsymbol&);
-    // bool is_valid()const { return( id > pid ); }
-    uint64_t raw()const { return( (uint64_t) pid * U1E9 + id ); }
-
-    EOSLIB_SERIALIZE( nsymbol, (id)(pid) )
-};
-
-bool operator==(const nsymbol& symb1, const nsymbol& symb2) { 
-    return( symb1.id == symb2.id && symb1.pid == symb2.pid );
-}
-
-
-struct nasset {
-    int64_t         amount;
-    nsymbol         symbol;
-
-    nasset() {}
-    nasset(const uint32_t& id): symbol(id), amount(0) {}
-    nasset(const uint32_t& id, const uint32_t& pid): symbol(id, pid), amount(0) {}
-    nasset(const uint32_t& id, const uint32_t& pid, const int64_t& am): symbol(id, pid), amount(am) {}
-    nasset(const int64_t& amt, const nsymbol& symb): amount(amt), symbol(symb) {}
-
-    nasset& operator+=(const nasset& quantity) { 
-        check( quantity.symbol.raw() == this->symbol.raw(), "nsymbol mismatch");
-        this->amount += quantity.amount; return *this;
-    } 
-    nasset& operator-=(const nasset& quantity) { 
-        check( quantity.symbol.raw() == this->symbol.raw(), "nsymbol mismatch");
-        this->amount -= quantity.amount; return *this; 
-    }
-
-    // bool is_valid()const { return symbol.is_valid(); }
-    
-    EOSLIB_SERIALIZE( nasset, (amount)(symbol) )
-};
-
 
 class [[eosio::contract("flon.ntoken")]] ntoken : public contract {
    public:
