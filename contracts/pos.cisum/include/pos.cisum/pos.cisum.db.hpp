0ac4fb5f4385bb0f4b96651f3d96a5cb57bf013d
haohaozaijianghu <yuanhaomail@yeah.net>
2025-08-20 17:33:41 +0800

    add pos.cisum

---
+++ contracts/pos.cisum/include/pos.cisum/pos.cisum.db.hpp
@@ -0,0 +1,134 @@
+#pragma once
+
+#include <eosio/asset.hpp>
+#include <eosio/privileged.hpp>
+#include <eosio/singleton.hpp>
+#include <eosio/system.hpp>
+#include <eosio/time.hpp>
+
+#include <utils.hpp>
+
+// #include <deque>
+#include <optional>
+#include <string>
+#include <map>
+#include <set>
+#include <type_traits>
+
+
+
+namespace flon {
+
+using namespace std;
+using namespace eosio;
+
+#define HASH256(str) sha256(const_cast<char*>(str.c_str()), str.size())
+
+#define TBL struct [[eosio::table, eosio::contract("pos.cisum")]]
+#define NTBL(name) struct [[eosio::table(name), eosio::contract("pos.cisum")]]
+
+namespace deposit_type {
+    static constexpr eosio::name TERM       = "term"_n;
+    static constexpr eosio::name DEMAND     = "demand"_n;
+}
+
+namespace interest_rate_scheme {
+    static constexpr eosio::name LADDER3    = "lad3"_n;     //12-mo ladder ir
+    static constexpr eosio::name LADDER2    = "lad2"_n;     //6-mo ladder ir
+    static constexpr eosio::name LADDER1    = "lad1"_n;     //3-mo ladder ir
+
+    static constexpr eosio::name LADDER31   = "lad31"_n;    //12-mo ladder ir
+    static constexpr eosio::name LADDER21   = "lad21"_n;    //6-mo ladder ir
+    static constexpr eosio::name LADDER11   = "lad11"_n;    //3-mo ladder ir
+
+    static constexpr eosio::name LOG1       = "log1"_n;
+
+    static constexpr eosio::name DEMAND1    = "dem1"_n;
+    static constexpr eosio::name DEMAND2    = "dem2"_n;
+    static constexpr eosio::name DEMAND3    = "dem3"_n;
+}
+
+NTBL("global") global_t {
+    name admin                              = "cisumadmin"_n;
+    name penalty_share_account              = "share.cisum"_n;
+    extended_symbol     principal_token;            //E.g. 8,AMAX@amax.token, can be set differently for diff contract
+    extended_symbol     interest_token;             //E.g. 8,AMAX@amax.token, can be set differently for diff contract
+    asset mini_deposit_amount;                      // 最小可存入本金
+    uint64_t share_pool_id                  = 0;    //to be set a value which has been set for this contract as a whole
+    uint64_t last_save_id                   = 0;
+
+    EOSLIB_SERIALIZE( global_t, (admin)(penalty_share_account)(principal_token)(interest_token)(mini_deposit_amount)
+                                (share_pool_id)(last_save_id) )
+
+};
+typedef eosio::singleton< "global"_n, global_t > global_singleton;
+
+struct plan_conf_s {
+    name                type;                       //存款类型：deposit_type::TERM / deposit_type::DEMAND
+    name                ir_scheme;                  //利率方案：interest_rate_scheme::*
+    uint64_t            deposit_term_days;          //定期天数 E.g. 365
+    bool                allow_advance_redeem;       //是否允许提前赎回
+    uint64_t            advance_redeem_fine_rate;   //提前赎回罚金比例E.g. 50% * 10000 = 5000
+    time_point_sec      effective_from;             //before which deposits are not allowed
+    time_point_sec      effective_to;               //after which deposits are not allowed but penalty split are allowed
+
+    EOSLIB_SERIALIZE( plan_conf_s,  (type)(ir_scheme)(deposit_term_days)
+                                    (allow_advance_redeem)(advance_redeem_fine_rate)
+                                    (effective_from)(effective_to) )
+};
+
+//scope: self
+TBL save_plan_t {
+    uint64_t            id;                         //PK
+    plan_conf_s         conf;                       // 方案配置
+    asset               deposit_available;          //当前在本方案中已存本金累计
+    asset               deposit_redeemed;           //已赎回本金累计
+    asset               interest_available;         //案内可用于发放的利息池
+    asset               interest_redeemed;          //已发放/提取的利息累计
+    time_point_sec      created_at;
+
+    save_plan_t() {}
+    save_plan_t(const uint64_t& i): id(i) {}
+
+    uint64_t primary_key()const { return id; }
+    uint64_t scope()const { return 0; }
+
+    typedef multi_index<"saveplans"_n, save_plan_t > tbl_t;
+
+    EOSLIB_SERIALIZE( save_plan_t,  (id)(conf)
+                                    (deposit_available)(deposit_redeemed)
+                                    (interest_available)(interest_redeemed)
+                                    (created_at) )
+
+};
+
+//Scope: account
+//Note: record will be deleted upon withdrawal/redemption
+TBL save_account_t {
+    uint64_t            save_id;              //PK
+    uint64_t            plan_id;
+    uint64_t            interest_rate;        //boost by 10000
+    asset               deposit_quant;        //存入本金
+    asset               interest_term_quant;  //total interest collectable upon term completion
+    asset               interest_collected;   //已领取利息累计
+    time_point_sec      created_at;
+    time_point_sec      term_ended_at;
+    time_point_sec      last_collected_at;    //上次领取利息时间
+
+
+    save_account_t() {}
+    save_account_t(const uint64_t& i): save_id(i) {}
+
+    uint64_t primary_key()const { return save_id; }
+    uint64_t by_plan()const { return plan_id; }
+
+    typedef multi_index<"saveaccounts"_n, save_account_t,
+        indexed_by<"planid"_n, const_mem_fun<save_account_t, uint64_t, &save_account_t::by_plan> >
+    > tbl_t;
+
+    EOSLIB_SERIALIZE( save_account_t,   (save_id)(plan_id)(interest_rate)(deposit_quant)(interest_term_quant)(interest_collected)
+                                        (created_at)(term_ended_at)(last_collected_at) )
+
+};
+
+} //namespace amax
---
+++ contracts/pos.cisum/src/pos.cisum.cpp
@@ -0,0 +1,381 @@
+#include <pos.cisum/pos.cisum.hpp>
+#include "safemath.hpp"
+#include <utils.hpp>
+
+
+static constexpr eosio::name active_perm        {"active"_n};
+
+#define TRANSFER(bank, to, quantity, memo) \
+    {	eosio::token::transfer_action act{ bank, { {_self, active_perm} } };\
+			act.send( _self, to, quantity , memo );}
+
+namespace flon {
+
+using namespace std;
+using namespace wasm::safemath;
+
+#define CHECKC(exp, code, msg) \
+   { if (!(exp)) eosio::check(false, string("[[") + to_string((int)code) + string("]] ") + msg); }
+
+   inline int64_t get_precision(const symbol &s) {
+      int64_t digit = s.precision();
+      CHECK(digit >= 0 && digit <= 18, "precision digit " + std::to_string(digit) + " should be in range[0,18]");
+      return calc_precision(digit);
+   }
+
+   inline int64_t get_precision(const asset &a) {
+      return get_precision(a.symbol);
+   }
+
+   //lad3: 12 mon, 365 days
+   inline uint64_t get_ir_ladder12m( const asset& quant ) {
+      if( quant.amount <= (1000 * get_precision(quant) ))   return 800;    // 8%
+      if( quant.amount <= (2000 * get_precision(quant) ))   return 1000;   // 10%
+                                                            return 1200;   // 12%
+   }
+
+   //lad2: 6 mon, 180 days
+   inline uint64_t get_ir_ladder6m( const asset& quant ) {
+      if( quant.amount <= (1000 * get_precision(quant) ))   return 680;    // 6.8%
+      if( quant.amount <= (2000 * get_precision(quant) ))   return 800;    // 8%
+                                                            return 1000;   // 10%
+   }
+
+   //lad1: 3 mon, 90 days
+   inline uint64_t get_ir_ladder3m( const asset& quant ) {
+      if( quant.amount <= (1000 * get_precision(quant) ))   return 600;    // 6%
+      if( quant.amount <= (2000 * get_precision(quant) ))   return 680;    // 6.8%
+                                                            return 800;    // 8%
+   }
+
+   inline uint64_t get_ir_dm1() {
+      return 100; // 1%
+   }
+   inline uint64_t get_ir_dm2() {
+      return 200; // 2%
+   }
+   inline uint64_t get_ir_dm3() {
+      return 300; // 3%
+   }
+
+   inline uint64_t get_interest_rate( const name& ir_scheme, const asset& quant ) {
+      switch( ir_scheme.value ) {
+         case interest_rate_scheme::LADDER1.value : return get_ir_ladder3m(quant);
+         case interest_rate_scheme::LADDER2.value : return get_ir_ladder6m(quant);
+         case interest_rate_scheme::LADDER3.value : return get_ir_ladder12m(quant);
+
+         case interest_rate_scheme::LADDER11.value : return 600;
+         case interest_rate_scheme::LADDER21.value : return 630;
+         case interest_rate_scheme::LADDER31.value : return 660;
+
+         case interest_rate_scheme::DEMAND1.value : return get_ir_dm1();
+         case interest_rate_scheme::DEMAND2.value : return get_ir_dm2();
+         case interest_rate_scheme::DEMAND3.value : return get_ir_dm3();
+         default:                                   return get_ir_dm1();
+      }
+   }
+
+   inline void _term_interest( const uint64_t interest_rate, const asset& deposit_quant,
+                              const uint64_t real_duration, const uint64_t& total_duraton, asset& interest ) {
+      CHECKC ( real_duration > 0, err::PARAM_ERROR, "invald param ")
+      interest.amount = mul_down( mul_down(interest_rate * 100, real_duration, total_duraton), deposit_quant.amount, PCT_BOOST * 100 );
+   }
+
+   // auto ext_symb = extended_symbol(AMAX, SYS_BANK);
+   // auto from = time_point_sec(1664246887); //::from_iso_string("2022-09-27T02:48:07+00:00");)
+   // auto to = time_point_sec(1666810087); //::from_iso_string("2022-10-27T00:00:00");
+   // _gstate.admin                 = "armoniaadmin"_n;
+   //   auto pc = plan_conf_s {
+   //       deposit_type::TERM,
+   //       interest_rate_scheme::LADDER1,
+   //       365,
+   //       true,
+   //       5000,
+   //       time_point_sec( default_plan_from ),
+   //       time_point_sec( default_plan_to )
+   //    };
+
+   void pos_cisum::init() {
+      CHECK(false, "disabled" )
+      require_auth( _self );
+
+      // auto old_save                 = save_account_t( 363 );
+      // auto old_acct                 = "aplxzxnb3he5"_n;
+      // auto new_acct                 = "apl3dnfgkzay"_n;
+      // _db.get( old_acct.value, old_save );
+      // auto tmp = old_save;
+      // _db.del( old_acct.value, old_save );
+
+      // auto new_save                 = save_account_t( 363 );
+      // new_save.plan_id              = tmp.plan_id;
+      // new_save.interest_rate        = tmp.interest_rate;
+      // new_save.deposit_quant        = tmp.deposit_quant;
+      // new_save.interest_term_quant  = tmp.interest_term_quant;
+      // new_save.interest_collected   = tmp.interest_collected;
+      // new_save.created_at           = tmp.created_at;
+      // new_save.term_ended_at        = tmp.term_ended_at;
+
+      // _db.set( new_acct.value, new_save, false );
+
+      // _gstate.principal_token       = ptoken;
+      // _gstate.interest_token        = itoken;
+      // _gstate.admin                 = admin;
+      // _gstate.mini_deposit_amount   = mini_deposit_amount;
+      // _gstate.share_pool_id         = share_pool_id;
+
+      // auto zero_pricipal            = asset(0, ptoken.get_symbol());
+      // auto zero_interest            = asset(0, itoken.get_symbol());
+
+      // auto plan = save_plan_t(1);
+      // _db.get( plan );
+      // plan.conf               = pc;
+      // plan.deposit_available  = zero_pricipal;
+      // plan.deposit_redeemed   = zero_pricipal;
+      // plan.interest_available = zero_interest;
+      // plan.interest_redeemed  = zero_interest;
+      // plan.created_at         = current_time_point();
+      // _db.set( plan );
+
+      // auto plan1 = save_plan_t(1);
+      // _db.get( plan1 );
+      // plan1.conf.ir_scheme = "lad1"_n;
+      // plan1.conf.deposit_term_days = 90;
+      // _db.set( plan1 );
+
+      // auto plan3 = save_plan_t(3);
+      // _db.get( plan3 );
+      // plan3.conf.ir_scheme = "lad3"_n;
+      // plan3.conf.deposit_term_days = 365;
+      // _db.set( plan3 );
+
+      // auto acct = "huanghuanghh"_n;
+
+      // auto save1 = save_account_t(1);
+      // _db.get( acct.value, save1 );
+      // save1.interest_term_quant.amount = 147945;
+      // save1.term_ended_at = save1.created_at + 90 * DAY_SECONDS;
+      // _db.set( acct.value, save1 );
+
+      // auto save3 = save_account_t(3);
+      // _db.get( acct.value, save3 );
+      // save3.interest_rate = 800;
+      // save3.interest_term_quant.amount = 800000;
+      // save3.term_ended_at = save3.created_at + 365 * DAY_SECONDS;
+      // _db.set( acct.value, save3 );
+
+   }
+
+   void pos_cisum::withdraw(const name& issuer, const name& owner, const uint64_t& save_id) {
+      require_auth( issuer );
+      // check(false, "under maintenance");
+
+      if ( issuer != owner ) {
+         CHECKC( issuer == _gstate.admin, err::NO_AUTH, "non-admin not allowed to withdraw others saving account" )
+      }
+
+      auto save_acct = save_account_t( save_id );
+      CHECKC( _db.get( owner.value, save_acct ), err::RECORD_NOT_FOUND, "account save not found" )
+
+      auto plan = save_plan_t( save_acct.plan_id );
+      CHECKC( _db.get( plan ), err::RECORD_NOT_FOUND, "plan not found: " + to_string(save_acct.plan_id) )
+
+      auto redeem_quant             = save_acct.deposit_quant;
+      if (plan.conf.type == deposit_type::TERM) {
+         auto save_termed_at        = save_acct.created_at + plan.conf.deposit_term_days * DAY_SECONDS;
+         auto now = current_time_point();
+         auto premature_withdraw = (now.sec_since_epoch() < save_termed_at.sec_since_epoch());
+         if (!plan.conf.allow_advance_redeem)
+            CHECKC( !premature_withdraw, err::NO_AUTH, "premature withdraw not allowed" )
+
+         if (premature_withdraw) {
+            auto unfinish_rate      = div( save_termed_at.sec_since_epoch() - now.sec_since_epoch(), plan.conf.deposit_term_days * DAY_SECONDS, PCT_BOOST );
+            auto penalty_amount     = mul_up( mul_up( save_acct.deposit_quant.amount, unfinish_rate, PCT_BOOST ), plan.conf.advance_redeem_fine_rate, PCT_BOOST );
+            auto penalty            = asset( penalty_amount, _gstate.principal_token.get_symbol() );
+            redeem_quant            -= penalty;
+            CHECKC( redeem_quant.amount > 0, err::INCORRECT_AMOUNT, "redeem amount not positive " )
+
+            TRANSFER( _gstate.principal_token.get_contract(), _gstate.penalty_share_account, penalty, owner.to_string() + ":" + to_string(_gstate.share_pool_id) )
+         }
+      }
+
+      plan.deposit_available        -= save_acct.deposit_quant;
+      plan.deposit_redeemed         += redeem_quant;
+      _db.set( plan );
+      _db.del( owner.value, save_acct );
+
+      TRANSFER( _gstate.principal_token.get_contract(), owner, redeem_quant, "redeem: " + to_string(save_id) )
+   }
+
+   void pos_cisum::collectint(const name& issuer, const name& owner, const uint64_t& save_id) {
+      require_auth( issuer );
+
+      // check(false, "under maintenance");
+
+      if ( issuer != owner ) {
+         CHECKC( issuer == _gstate.admin, err::NO_AUTH, "non-admin not allowed to collect others saving interest" )
+      }
+
+      auto save_acct = save_account_t( save_id );
+      CHECKC( _db.get( owner.value, save_acct ), err::RECORD_NOT_FOUND, "account save not found" )
+
+      auto plan = save_plan_t( save_acct.plan_id );
+      CHECKC( _db.get( plan ), err::RECORD_NOT_FOUND, "plan not found: " + to_string(save_acct.plan_id) )
+
+      //interest_rate_ = interest_rate * ( (now - deposited_at) / day_secs / 365 )
+      if (save_acct.last_collected_at == time_point())
+         save_acct.last_collected_at = save_acct.created_at;
+
+      auto now                = current_time_point();
+      auto elapsed_sec        = now.sec_since_epoch() - save_acct.last_collected_at.sec_since_epoch();
+      CHECKC( elapsed_sec > DAY_SECONDS, err::TIME_PREMATURE, "less than 24 hours since last interest collection time" )
+      auto total_elapsed_sec  = now.sec_since_epoch() - save_acct.created_at.sec_since_epoch();
+
+      auto interest           = asset( 0, _gstate.interest_token.get_symbol() );
+      _term_interest(save_acct.interest_rate, save_acct.deposit_quant, total_elapsed_sec, YEAR_DAYS * DAY_SECONDS, interest );
+      if (interest > save_acct.interest_term_quant)
+         interest = save_acct.interest_term_quant;
+
+      auto interest_due       = interest - save_acct.interest_collected;
+
+      CHECKC( interest_due.amount > 0, err::NOT_POSITIVE, "interest due amount is zero" )
+      TRANSFER( _gstate.interest_token.get_contract(), owner, interest_due, "interest: " + to_string(save_id) )
+
+      save_acct.interest_collected  += interest_due;
+      save_acct.last_collected_at   = now;
+      _db.set( owner.value, save_acct );
+
+      CHECKC( plan.interest_available > interest_due, err::NOT_POSITIVE, "insufficient available interest to collect" )
+
+      plan.interest_available       -= interest_due;
+      plan.interest_redeemed        += interest_due;
+
+      _db.set( plan );
+
+      _int_coll_log(owner, save_acct.save_id, plan.id, interest_due,  time_point_sec( current_time_point() ));
+
+   }
+
+
+   /**
+    * @brief send nasset tokens into nftone marketplace
+    *
+    * @param from
+    * @param to
+    * @param quantity
+    * @param memo: two formats:
+    *       0) <NULL>               -- by saver to deposit to plan_id=1
+    *       1) refuel:$plan_id      -- by admin to deposit interest quantity
+    *       2) deposit:$plan_id     -- by saver to deposit saving quantity to his or her own account
+    *
+    */
+   void pos_cisum::ontransfer(const name& from, const name& to, const asset& quant, const string& memo) {
+      CHECKC( from != to, err::ACCOUNT_INVALID, "cannot transfer to self" );
+
+      // check(false, "under maintenance");
+
+      if (from == get_self() || to != get_self()) return;
+
+      auto token_bank = get_first_receiver();
+
+      vector<string_view> memo_params = split(memo, ":");
+      if( memo_params.size() == 2 && memo_params[0] == "refuel" ) {
+         auto plan_id = to_uint64(memo_params[1], "refuel plan");
+         auto plan = save_plan_t( plan_id );
+         CHECKC( _db.get( plan ), err::RECORD_NOT_FOUND, "plan id not found: " + to_string( plan_id ) )
+         CHECKC( _gstate.interest_token.get_contract() == token_bank, err::CONTRACT_MISMATCH, "interest token contract mismatches" )
+
+         plan.interest_available += quant;
+         _db.set( plan );
+         _int_refuel_log(from, plan_id, quant, current_time_point());
+
+      } else {
+         CHECKC( _gstate.mini_deposit_amount <= quant, err::INCORRECT_AMOUNT, "deposit amount too small" )
+
+         uint64_t plan_id = 1;   //default 1st-plan
+         if (memo_params.size() == 2 && memo_params[0] == "deposit")
+            plan_id = to_uint64(memo_params[1], "deposit plan");
+
+         auto now = time_point_sec(current_time_point());
+         auto plan = save_plan_t( plan_id );
+         CHECKC( _db.get( plan ), err::RECORD_NOT_FOUND, "plan id not found: " + to_string( plan_id ) )
+         CHECKC( _gstate.principal_token.get_contract() == token_bank, err::CONTRACT_MISMATCH, "deposit token contract mismatches" )
+         CHECKC( plan.conf.effective_from <= now, err::PLAN_INEFFECTIVE, "plan not effective yet" )
+         CHECKC( plan.conf.effective_to   >= now, err::PLAN_INEFFECTIVE, "plan expired already" )
+
+         plan.deposit_available        += quant;
+         _db.set( plan );
+
+         // auto accts                    = save_account_t::tbl_t(_self, from.value);
+         auto save_acct                = save_account_t( ++_gstate.last_save_id );
+         save_acct.plan_id             = plan_id;
+         save_acct.interest_rate       = get_interest_rate( plan.conf.ir_scheme, quant);
+         save_acct.interest_term_quant = asset(0, _gstate.interest_token.get_symbol());
+         _term_interest( save_acct.interest_rate, quant, plan.conf.deposit_term_days, YEAR_DAYS, save_acct.interest_term_quant );
+
+         save_acct.deposit_quant       = quant;
+         save_acct.interest_collected  = asset( 0, _gstate.interest_token.get_symbol() );
+         save_acct.created_at          = now;
+         save_acct.term_ended_at       = now + plan.conf.deposit_term_days * DAY_SECONDS;
+
+         _db.set( from.value, save_acct, false );
+         //TODO call addshare
+      }
+   }
+
+   void pos_cisum::setplan(const uint64_t& pid, const plan_conf_s& pc) {
+      require_auth( _gstate.admin );
+
+      auto plan = save_plan_t(pid);
+      bool plan_existing = _db.get( plan );
+
+      plan.conf.type                   = pc.type;
+      plan.conf.ir_scheme              = pc.ir_scheme;
+      plan.conf.deposit_term_days      = pc.deposit_term_days;
+      plan.conf.allow_advance_redeem   = pc.allow_advance_redeem;
+      plan.conf.advance_redeem_fine_rate = pc.advance_redeem_fine_rate;
+      plan.conf.effective_from         = pc.effective_from;
+      plan.conf.effective_to           = pc.effective_to;
+
+      if (!plan_existing) {
+         auto zero_pricipal            = asset(0, _gstate.principal_token.get_symbol());
+         auto zero_interest            = asset(0, _gstate.interest_token.get_symbol());
+         plan.deposit_available        = zero_pricipal;
+         plan.deposit_redeemed         = zero_pricipal;
+         plan.interest_available       = zero_interest;
+         plan.interest_redeemed        = zero_interest;
+         plan.created_at               = current_time_point();
+      }
+
+      _db.set( plan );
+   }
+
+   void pos_cisum::delplan(const uint64_t& pid) {
+      require_auth( _gstate.admin );
+
+      auto plan = save_plan_t(pid);
+      CHECKC(_db.get( plan ), err::RECORD_NOT_FOUND, "plan not exist: " + to_string(pid) )
+
+      _db.del( plan );
+   }
+
+   void pos_cisum::_int_coll_log(const name& account, const uint64_t& account_id, const uint64_t& plan_id, const asset &quantity, const time_point& created_at) {
+      pos_cisum::interest_withdraw_log_action act{ _self, { {_self, active_perm} } };
+      act.send( account, account_id, plan_id, quantity, created_at );
+   }
+
+   void pos_cisum::_int_refuel_log(const name& refueler, const uint64_t& plan_id, const asset &quantity, const time_point& created_at) {
+      pos_cisum::intrefuellog_action act{ _self, { {_self, active_perm} } };
+      act.send( refueler, plan_id, quantity, created_at );
+   }
+
+   void pos_cisum::intrefuellog(const name& refueler, const uint64_t& plan_id, const asset &quantity, const time_point& created_at) {
+      require_auth(get_self());
+      require_recipient(refueler);
+   }
+
+   void pos_cisum::intcolllog(const name& account, const uint64_t& account_id, const uint64_t& plan_id, const asset &quantity, const time_point& created_at) {
+      require_auth(get_self());
+      require_recipient(account);
+   }
+
+} //namespace flon
