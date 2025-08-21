db4133b8feb48dd16701a4d1137d4ba02ce967fc
haohaozaijianghu <yuanhaomail@yeah.net>
2025-08-21 09:51:31 +0800

    update

--- contracts/show.cisum/include/cvticket.nft.hpp
+++ contracts/show.cisum/include/cvticket.nft.hpp
@@ -2,6 +2,8 @@
 #include <eosio/eosio.hpp>
 #include <string>
 #include <vector>
+using std::string;
+using std::vector;
 
 namespace flon {
 using namespace eosio;
@@ -68,11 +70,17 @@ class [[eosio::contract("cvticket.nft")]] cvticket : public contract {
 public:
   using contract::contract;
 
-  // 仅声明 transfer 动作及其 action_wrapper
-  [[eosio::action]]
-  void transfer(const name& from, const name& to, const std::vector<nasset>& assets, const std::string& memo);
+  ACTION create( const name& issuer, const int64_t& maximum_supply, const nsymbol& symbol, const string& token_uri, const name& ipowner );
+
+  ACTION issue( const name& to, const nasset& quantity, const string& memo );
+
+  ACTION transfer(const name& from, const name& to, const std::vector<nasset>& assets, const std::string& memo);
+
+
 
   using transfer_action = action_wrapper<"transfer"_n, &cvticket::transfer>;
+  using create_action   = action_wrapper<"create"_n, &cvticket::create>;
+  using issue_action    = action_wrapper<"issue"_n, &cvticket::issue>;
 };
 
 } // namespace flon
--- tests/show.cisum/1-test.sh
+++ tests/show.cisum/1-test.sh
@@ -1,4 +1,4 @@
-show_con=show11.cisum
+show_con=show12.cisum
 mreg flon $show_con flonian
 mtran flonian $show_con "100 FLON"
 mset $show_con show.cisum
@@ -8,51 +8,39 @@ mcli set account permission $show_con active --add-code
 
 mpush $ticket_ntoken  addwhitelist '["'"${show_con}"'"]'   -p $ticket_ntoken
 
-#普通票
-mpush $ticket_ntoken create '[
-  "'"${show_con}"'",
-  10000000000,
-  {"id": 010010, "pid": 0021010001},
-  "ipfs://ticket/silver.json",
-  "'"${show_con}"'"
-]' -p $show_con
-
-
-mpush $ticket_ntoken issue '[
-  "'"${show_con}"'",
-  {"amount": 10000000, "symbol": {"id": 010010, "pid": 0021010001}},
-  "bootstrap batch"
-]' -p $show_con
-
-
-#合影票
-mpush $ticket_ntoken create '[
-  "'"${show_con}"'",
-  10000000000,
-  {"id": 010012, "pid": 0021010001},
-  "ipfs://ticket/silver2.json",
-  "'"${show_con}"'"
-]' -p $show_con
-
-
-mpush $ticket_ntoken issue '[
-  "'"${show_con}"'",
-  {"amount": 10000000, "symbol": {"id": 010012, "pid": 0021010001}},
-  "bootstrap batch"
-]' -p $show_con
-
-
-
-
-
-
-
-
 mpush $show_con init '["flonian", "cvticket.nft"]' -p $show_con
 
 mpush $show_con addshowadm '["flonian"]' -p flonian
 
 mpush $show_con addshowadm '["myadmin"]' -p flonian
+
+
+#普通票
+mpush $show_con ntcreate '[
+  10000000000,
+  {"id": 010013, "pid": 0021010001},
+  "ipfs://ticket/silver4.json"
+]' -p flonian
+
+
+mpush $show_con ntissue '[
+  {"amount": 10000000, "symbol": {"id": 010013, "pid": 0021010001}},
+  "bootstrap batch"
+]' -p flonian
+
+
+#合影票
+mpush $show_con ntcreate '[
+  10000000000,
+  {"id": 010014, "pid": 0021010001},
+  "ipfs://ticket/silver5.json"
+]' -p flonian
+
+
+mpush $show_con ntissue '[
+  {"amount": 10000000, "symbol": {"id": 010014, "pid": 0021010001}},
+  "bootstrap batch"
+]' -p flonian
 
 
 
@@ -62,8 +50,8 @@ mpush $show_con addchecker '[1,"myadmin"]'   -p flonian
 mpush $show_con newshow '["concert1", true, true, "2025-09-01T00:00:00", "2025-09-10T23:59:59", "2025-09-15T18:00:00", "2025-09-15T22:00:00", "onshelf"]' -p myadmin
 mpush $show_con addchecker '[2,"myadmin"]'   -p flonian
 
-mpush $show_con newticket '[1, {"id":10010,"pid":21010001}, {"id":0,"pid":0}, "普通票", "100.00 USDT", 100, "running"]' -p flonian
+mpush $show_con newticket '[1, {"id":010013,"pid":21010001}, {"id":0,"pid":0}, "普通票", "100.00 USDT", 100, "running"]' -p flonian
 
 
-mpush $show_con issue '["gahbnbehaskk",1,21010001000010010,1,"send to gahbnbehaskk 1 ticket NFT"]' -p flonian
+mpush $show_con issue '["gahbnbehaskk",1,21010001000010013,1,"send to gahbnbehaskk 1 ticket NFT"]' -p flonian
 
