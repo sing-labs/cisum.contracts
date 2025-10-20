6c548bc7e7ccf84c43fa1919f60aec8f5d30f7be
Rich <galaxychainlabs@gmail.com>
2025-10-20 14:02:35 +0800

    rm global existence check

--- contracts/flon.auth/src/flon.auth.cpp
+++ contracts/flon.auth/src/flon.auth.cpp
@@ -35,8 +35,8 @@ bool flonauth::role_exists(const name& role) const {
 
 void flonauth::init(const name& admin) {
     require_auth(get_self());
-    CHECKC(!_global.exists() || _gstate.admin.value == 0,
-           err::INVALID_FORMAT, "already initialized");
+    // CHECKC(!_global.exists() || _gstate.admin.value == 0,
+    //        err::INVALID_FORMAT, "already initialized");
     CHECKC(is_account(admin), err::ACCOUNT_INVALID, "admin not exist");
     _gstate.admin = admin;
 }
