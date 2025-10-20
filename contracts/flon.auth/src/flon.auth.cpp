005563a8e00a689e179f38cd02f3aefc0f74f0c4
Rich <galaxychainlabs@gmail.com>
2025-10-20 14:13:33 +0800

    update

--- contracts/flon.auth/src/flon.auth.cpp
+++ contracts/flon.auth/src/flon.auth.cpp
@@ -37,7 +37,6 @@ void flonauth::init(const name& admin) {
     require_auth(get_self());
     // CHECKC(!_global.exists() || _gstate.admin.value == 0,
     //        err::INVALID_FORMAT, "already initialized");
-    CHECKC(is_account(admin), err::ACCOUNT_INVALID, "admin not exist");
     _gstate.admin = admin;
 }
 
